a9a8cd6c3b835d134e70ebdeb7a3999e22ee4996
JUB_Yoush <yoush1337@gmail.com>
2025-05-22 15:24:48 -0400

    why is that instruction sending me to brazil

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use bit_vec::BitVec;
 use grid::*;
+use std::collections::HashSet;
 use std::ops::Range;
 use std::{
     fs,
@@ -14,7 +15,17 @@ struct Point {
     pub x: u8,
     pub y: u8,
 }
-
+// fn conf() -> Conf {
+//     Conf {
+//         window_title: String::from("Macroquad"),
+//         window_width: 1260,
+//         window_height: 768,
+//         fullscreen: false,
+//         ..Default::default()
+//     }
+// }
+//
+// #[macroquad::main(conf)]
 //font byte representation
 // 0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
 // 0x20, 0x60, 0x20, 0x20, 0x70, // 1
@@ -35,6 +46,7 @@ struct Point {
 
 trait UpdateDisplay {
     fn update_display(&mut self, x: usize, y: usize, state: bool) {}
+    fn render_display(&self, pixel_scale: usize) {}
 }
 
 impl UpdateDisplay for Grid<bool> {
@@ -43,6 +55,82 @@ impl UpdateDisplay for Grid<bool> {
         let mut row = row.unwrap();
         row[y] = state;
         self.insert_row(x, row);
+    }
+    fn render_display(&self, pixel_scale: usize) {
+        for (point, on) in self.indexed_iter() {
+            //println!("pixel at {}x{} is {}", point.0, point.1, on);
+            draw_rectangle(
+                (point.0 * pixel_scale) as f32,
+                (point.1 * pixel_scale) as f32,
+                pixel_scale as f32,
+                pixel_scale as f32,
+                match on {
+                    true => WHITE,
+                    false => BLACK,
+                },
+            );
+        }
+    }
+}
+
+impl Cpu {
+    fn fetch(&self) -> Option<&[u8]> {
+        // appeasing the borrow checker
+        let pc = self.pc - 2;
+        print!("pc = {:#04X} ", pc);
+        if pc >= self.rom.len() - 1 {
+            None
+        } else {
+            Some(self.rom.get(pc..=pc + 1)?)
+        }
+    }
+    // we don't really need the cpu to implement decode because it's a static function
+    fn execute(&mut self, instruction: Instruction) {
+        use Instruction::*;
+        match instruction {
+            ClearScreen => {
+                self.screen_grid = Grid::new(64, 32);
+            }
+            Jump(addr) => {
+                println!("setting pc to {:#04X}", addr);
+                self.pc = addr as usize;
+            }
+            Add { value, address } => {
+                self.registers[address as usize] += value;
+            }
+            Set { value, address } => {
+                self.registers[address as usize] = value;
+            }
+            SetIReg(value) => {
+                self.index_register = value;
+            }
+            Draw {
+                addrx,
+                addry,
+                height,
+            } => {
+                let mut x = self.registers[addrx as usize] % 64;
+                let mut y = self.registers[addry as usize] % 32;
+                let mut vf = 0;
+                for i in 0..=height {
+                    let byte = self.ram[self.index_register as usize + i as usize];
+                    let bit_vec = BitVec::from_bytes(&[byte]);
+                    for bit in bit_vec.iter() {
+                        if bit && *self.screen_grid.get(y, x).unwrap() {
+                            self.screen_grid
+                                .update_display(x as usize, y as usize, false);
+                            vf = 1;
+                        } else if bit && !self.screen_grid.get(y, x).unwrap() {
+                            self.screen_grid
+                                .update_display(x as usize, y as usize, true);
+                        }
+                        x += 1;
+                    }
+                    y += 1;
+                }
+            }
+            _ => panic!("didn't implement this yet."),
+        }
     }
 }
 
@@ -76,95 +164,70 @@ enum Instruction {
         height: u8,
     },
 }
+
+#[derive(Clone)]
+struct Cpu {
+    screen_grid: Grid<bool>,
+    pc: usize,
+    registers: [u8; 16],
+    ram: [u8; 4000],
+    stack: Vec<u16>,
+    index_register: u16,
+    rom: Vec<u8>,
+}
+
 #[macroquad::main("chip-8")]
 async fn main() -> Result<(), std::io::Error> {
     // hardware components
-    let mut pc: usize = 0;
-    let mut screen_grid: Grid<bool> = Grid::new(64, 32);
-    let mut ram: [u8; 4000] = [0; 4000];
-    let mut stack: Vec<u16> = Vec::new();
-    let mut rom: Vec<u8> = fs::read("roms/ibm.ch8")?;
-
+    let mut cpu = Cpu {
+        pc: 0,
+        screen_grid: Grid::new(64, 32),
+        registers: [0; 16],
+        ram: [0; 4000],
+        stack: Vec::new(),
+        index_register: 0,
+        rom: fs::read("roms/ibm.ch8")?,
+    };
     // rendering
+    clear_background(BLACK);
     let pixel_scale: usize = 16;
-    println!("{:?}", rom);
+    println!("{:?}", cpu.rom);
     // main loop
 
-    clear_background(BLACK);
     'running: loop {
         request_new_screen_size(64. * pixel_scale as f32, 32. * pixel_scale as f32);
-        screen_grid.update_display(10, 8, true);
-        screen_grid.update_display(9, 8, true);
-        screen_grid.update_display(8, 8, true);
-        render_display(&screen_grid, pixel_scale);
-
-        // let instruction_slice = fetch(pc, &rom);
-        // pc += 2;
-        // match instruction_slice {
-        //     Some(value) => {
-        //         let instruction = decode(value);
-        //         if instruction.is_err() {
-        //             panic!("fakeass instruction")
-        //         }
-        //         execute(instruction.unwrap());
-        //     }
-        //     None => {
-        //         println!("we've reached the end of the rom.");
-        //         panic!("balls");
-        //     }
-        // }
-
+        cpu.screen_grid.render_display(pixel_scale);
+        cpu.pc += 2;
+        let instruction_slice = cpu.fetch();
+        match &instruction_slice {
+            Some(value) => {
+                println!(
+                    "INSTRUCTION AT {:#04X}: {:#04X},{:#04X}",
+                    cpu.pc - 2,
+                    value.first().unwrap(),
+                    value.last().unwrap()
+                );
+                let instruction = decode(value);
+                if instruction.is_err() {
+                    panic!("fakeass instruction")
+                }
+                cpu.execute(instruction.unwrap());
+            }
+            None => {
+                println!("we've reached the end of the rom.");
+                panic!("balls")
+            }
+        }
+        if is_key_pressed(KeyCode::Escape) {
+            break 'running;
+        }
         next_frame().await
     }
-
     Ok(())
 }
 
-fn update_display(x: usize, y: usize, state: bool, mut screen_grid: Grid<bool>) -> Grid<bool> {
-    let row = screen_grid.remove_row(y);
-    let mut row = row.unwrap();
-    row[x] = state;
-    screen_grid.insert_row(y, row);
-    // appease the borrow checker
-    screen_grid
-}
-
-fn render_display(screen_grid: &Grid<bool>, pixel_scale: usize) {
-    for (point, on) in screen_grid.indexed_iter() {
-        //println!("pixel at {}x{} is {}", point.0, point.1, on);
-        draw_rectangle(
-            (point.0 * pixel_scale) as f32,
-            (point.1 * pixel_scale) as f32,
-            pixel_scale as f32,
-            pixel_scale as f32,
-            match on {
-                true => WHITE,
-                false => BLACK,
-            },
-        );
-    }
-}
-
-fn fetch(pc: usize, rom: &[u8]) -> Option<&[u8]> {
-    if pc >= rom.len() - 1 {
-        None
-    } else {
-        Some(rom.get(pc..=pc + 1)?)
-    }
-}
-
 fn decode(instruction_bytes: &[u8]) -> Result<Instruction, &'static str> {
-    /*
-    println!(
-        "{:#01x} | {:#01x} \n",
-        instruction_bytes.get(0)?,
-        instruction_bytes.get(1)?
-    );
-    */
-
     use Instruction::*;
-    // let first_byte = instruction_bytes.first()?;
-    // let second_byte = instruction_bytes.last()?;
     let bit_vec = BitVec::from_bytes(instruction_bytes);
 
     match hex_digit(bit_vec.clone(), 0) {
@@ -173,7 +236,7 @@ fn decode(instruction_bytes: &[u8]) -> Result<Instruction, &'static str> {
             0xE => Ok(Return),
             //0xE0
             0x0 => Ok(ClearScreen),
-            _ => Err("invalid 0x0 instruction instruction"),
+            _ => Err("invalid 0x0 instruction"),
         },
         0x1 => Ok(Jump(bits_to_value(
             bit_vec.clone(),
@@ -190,6 +253,15 @@ fn decode(instruction_bytes: &[u8]) -> Result<Instruction, &'static str> {
         0x7 => Ok(Add {
             value: (bits_to_value(bit_vec.clone(), Range { start: 8, end: 16 }) as u8),
             address: hex_digit(bit_vec.clone(), 1),
+        }),
+        0xA => Ok(SetIReg(bits_to_value(
+            bit_vec.clone(),
+            Range { start: 4, end: 16 },
+        ))),
+        0xD => Ok(Draw {
+            addrx: hex_digit(bit_vec.clone(), 1) as u16,
+            addry: hex_digit(bit_vec.clone(), 2) as u16,
+            height: hex_digit(bit_vec.clone(), 3),
         }),
         _ => Err("invalid instruction"),
     }
@@ -230,15 +302,6 @@ fn hex_digit(bits: BitVec, offset: u8) -> u8 {
         _ => panic!("invalid offset idiot"),
     }
 }
-
-fn execute(instruction: Instruction) {
-    use Instruction::*;
-    // match instruction {
-    //     ClearScreen => clear_screen(d, thread),
-    //     _ => println!("unimplemented"),
-    // }
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
